6a4fa3686e9c98fe6a4a37f6826a3537c7241969
SHADOWZXCV <faded.lover.shadows@gmail.com>
2022-09-08 03:48:13 +0000

    reimplemented 101 quote c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,11 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\""
-	"- Dora Korpar, 2015-10-19\n");
+	char str1[] = "and that piece of art is useful\"";
+	char str2[] = " - Dora Korpar, 2015-10-19\n";
+
+	fwrite(str1, 1, sizeof(str1), stderr);
+	fwrite(str2, 1, sizeof(str2), stderr);
 
 	return (1);
 }
