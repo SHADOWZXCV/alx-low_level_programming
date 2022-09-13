eca343861b24a131b5bc62662372727366178c60
SHADOWZXCV <faded.lover.shadows@gmail.com>
2022-09-13 06:58:53 +0000

    task-5: added missing prints

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign - checks the sign of a number
- * 
+ *
  * @n: the number to be checked
  *
  * Return: 0 if it is zero, 1 if positive, -1 if negative
@@ -10,9 +10,19 @@
 int print_sign(int n)
 {
 	if (n > 0)
+	{
+		_putchar('+');
+
 		return (1);
+	}
 	else if (n < 0)
+	{
+		_putchar('-');
+
 		return (-1);
-	else
-		return (0);
+	}
+
+	_putchar('0');
+
+	return (0);
 }
