07896bbfb8766c7914eacc0ca0879f5dc0b1f8e0
SHADOWZXCV <faded.lover.shadows@gmail.com>
2022-10-20 19:04:30 +0000

    Task-5: commit #1

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * runFirst - a function prints something before running main
+*/
+void runFirst(void)
+{
+	printf("You're beat! and yet");
+	printf(", you must allow,\nI bore my house upon my back!\n");
+}
