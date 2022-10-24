8459b4f2abfab94116a8cc3ea34027a73c740993
SHADOWZXCV <faded.lover.shadows@gmail.com>
2022-10-24 19:07:42 +0000

    Task-0&7: commit #2&#1

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,7 +8,12 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
-	const listint_t *ptr = h;
+	const listint_t *ptr;
+
+	if (h == NULL)
+		return (0);
+
+	ptr = h;
 
 	while (ptr)
 	{
---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list.
+ * @head: head of the linked list.
+ * @index: index of the node, starting with 0.
+ * Return: pointer to the node, otherwise, NULL is returned.
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *ptr;
+
+	if (!head)
+		return (NULL);
+
+	ptr = head;
+	
+	while (ptr && index != 0)
+	{
+		ptr = ptr->next;
+		index--;
+	}
+
+	if (index != 0)
+		return (NULL);
+
+	return (ptr);
+}
