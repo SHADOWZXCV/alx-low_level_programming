05dd8606ecac8b180aecde4c1ddbcc98fe16d154
SHADOWZXCV <faded.lover.shadows@gmail.com>
2022-10-24 19:28:07 +0000

    Task-9: commit #2 - added nullptr checker.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		idx--;
 	}
 
-	if (idx != NODE_BEFORE_POSITION)
+	if (idx != NODE_BEFORE_POSITION || !ptr)
 	{
 		free(newNode);
 		return (NULL);
